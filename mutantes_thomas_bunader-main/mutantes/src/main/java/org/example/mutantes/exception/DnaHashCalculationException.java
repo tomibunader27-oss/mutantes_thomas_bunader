edece0d03399ceda736775b328463e24cda2d9ad
tomibunader27-oss <tomibunader27@gmail.com>
2025-11-25 20:53:17 -0300

    Delete mutantes_thomas_bunader-main directory

--- mutantes_thomas_bunader-main/mutantes/src/main/java/org/example/mutantes/exception/DnaHashCalculationException.java
+++
@@ -1,8 +0,0 @@
-package org.example.mutantes.exception;
-
-public class DnaHashCalculationException extends RuntimeException {
-
-    public DnaHashCalculationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
