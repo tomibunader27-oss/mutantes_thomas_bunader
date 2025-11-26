17aa614f65a4f5e631710ebee7a99c451031123a
tomibunader27-oss <tomibunader27@gmail.com>
2025-11-25 21:06:30 -0300

    Add files via upload

---
+++ src/main/java/org/example/mutantes/exception/DnaHashCalculationException.java
@@ -0,0 +1,8 @@
+package org.example.mutantes.exception;
+
+public class DnaHashCalculationException extends RuntimeException {
+
+    public DnaHashCalculationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/test/java/org/example/mutantes/StatsControllerTest.java
@@ -0,0 +1,39 @@
+package org.example.mutantes;
+
+import org.example.mutantes.DTO.StatsResponse;
+import org.example.mutantes.controller.StatsController;
+import org.example.mutantes.service.StatsService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(StatsController.class)
+class StatsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StatsService statsService;
+
+    @Test
+    void testGetStats() throws Exception {
+        StatsResponse mockStats = new StatsResponse(40, 100, 0.4);
+        when(statsService.getStats()).thenReturn(mockStats);
+
+        mockMvc.perform(get("/stats")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.count_mutant_dna").value(40))
+                .andExpect(jsonPath("$.count_human_dna").value(100))
+                .andExpect(jsonPath("$.ratio").value(0.4));
+    }
+}
